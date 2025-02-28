5ad69232473d891875046119acaea184c6538880
art-sparrow <theophilousworship@gmail.com>
2025-02-28 17:08:55 +0300

    Set home to `DecisionScreen`

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:taskify/core/utils/router.dart';
-import 'package:taskify/features/auth/presentation/login_screen.dart';
+import 'package:taskify/features/decision/presentation/decision_screen.dart';
 import 'package:taskify/l10n/l10n.dart';
 
 class App extends StatelessWidget {
@@ -13,7 +13,7 @@ class App extends StatelessWidget {
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       onGenerateRoute: TaskifyRouter.handleRoute,
-      home: LoginScreen(),
+      home: DecisionScreen(),
     );
   }
 }
